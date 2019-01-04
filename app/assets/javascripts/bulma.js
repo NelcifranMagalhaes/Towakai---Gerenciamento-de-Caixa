0f07fab0218751fe39114d134ed761e9538d23d8
nelcifran <nelcifranpires@gmail.com>
2019-01-04 18:50:24 -0300

    some changes in layoult of bulma

---
+++ app/assets/javascripts/bulma.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+
+ });
