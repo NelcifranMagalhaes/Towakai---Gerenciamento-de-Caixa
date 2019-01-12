aab370cda82d2a7eafe67d840468b6be2ede31b5
nelcifran <nelcifranpires@gmail.com>
2019-01-12 17:24:57 -0300

    changes for deploy

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module InventoryTowakai
     config.load_defaults 5.2
     config.i18n.default_locale ="pt-BR"
     config.assets.enabled = true
+    Faker::Config.locale = "pt-BR"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
 
 Client.destroy_all
+puts "Deletando Clientes"
 Product.destroy_all
+puts "Deletando Produtos"
 
+Sale.destroy_all
+puts "Deletando Vendas"
+
+Order.destroy_all
+puts "Deletando Orders"
+
+puts "Criados Clientes"
 30.times do |index|
-	Client.create!(name: Faker::OnePiece.character,address: Faker::OnePiece.location)
+	Client.create!(name: Faker::Name.name,address: Faker::Address.full_address)
+end
+puts "Criados Produtos"
+30.times do |index|
+	Product.create!(name: Faker::Commerce.product_name,
+		cost_price: Faker::Number.between(1, 100),tax: Faker::Number.between(1, 100),
+		sale_price: Faker::Number.between(100, 1000),quantity: Faker::Number.between(1, 100))
 end
 
-# 30.times do |index|
-# 	Product.create!(name: Faker::Commerce.product_name,cost_price: Faker::Number.decimal(2),tax: Faker::Number.decimal(2),sale_price: Faker::Number.decimal(2),quantity: Faker::Number.between(1, 100))
-# end
