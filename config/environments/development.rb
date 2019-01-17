6b354a2fccba2c7951b3f4d81b24e88c5758c7b4
nelcifran <nelcifranpires@gmail.com>
2019-01-17 11:02:39 -0300

    changes for email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,6 +52,19 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  #para o devise
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
