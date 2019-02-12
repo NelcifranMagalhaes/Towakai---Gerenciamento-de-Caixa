78e4300705182810bd746452befe0a0416833e65
nelcifran <nelcifranpires@gmail.com>
2019-02-12 14:11:20 -0300

    added in index of products, types of product

--- app/assets/javascripts/statistics.js
+++ app/assets/javascripts/statistics.js
@@ -1,76 +1,76 @@
-$(document).ready(function() {
-    let sales = $('.temp_information').data('temp');
-    let year_list = [];
-    let value_list = [];
-    let all_list = [];
-    let count = 0;
+// $(document).ready(function() {
+//     let sales = $('.temp_information').data('temp');
+//     let year_list = [];
+//     let value_list = [];
+//     let all_list = [];
+//     let count = 0;
 
-    for (x in sales) {
-        let date = new Date(x);
-        let year = date.getFullYear();
-        // console.log(sales[x]);
-        value_list.push(sales[x]);
-
-        if (!year_list.includes(year)) {
-            year_list.push({name: year,data: value_list});
-            if (true) {}
-        }
-
-
-        console.log(sales[x]);
-        console.log(x);
-    }
-
-    all_list.push({name: year,data: value_list})
-    console.log(year_list);
+//     for (x in sales) {
+//         let date = new Date(x);
+//         let year = date.getFullYear();
+//         // console.log(sales[x]);
+//         value_list.push(sales[x]);
+//         year_list.push({name: year,data: 0});
+//         if (year_list >0) {
+            
+//             if (year_list[count].name == year) {
+//                 year_list[count].data = sales[x];
+//             }
+//         }
+//         console.log(count);
+//         // console.log(sales[x]);
+//         // console.log(x);
+//         count += 1;
+//     }
+//     console.log(year_list);
     
-    Highcharts.chart('container', {
+//     Highcharts.chart('container', {
 
-        title: {
-            text: 'Solar Employment Growth by Sector, 2010-2016'
-        },
+//         title: {
+//             text: 'Solar Employment Growth by Sector, 2010-2016'
+//         },
 
-        subtitle: {
-            text: 'Source: thesolarfoundation.com'
-        },
+//         subtitle: {
+//             text: 'Source: thesolarfoundation.com'
+//         },
 
-        yAxis: {
-            title: {
-                text: 'Number of Employees'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
+//         yAxis: {
+//             title: {
+//                 text: 'Number of Employees'
+//             }
+//         },
+//         legend: {
+//             layout: 'vertical',
+//             align: 'right',
+//             verticalAlign: 'middle'
+//         },
 
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2018
-            }
-        },
+//         plotOptions: {
+//             series: {
+//                 label: {
+//                     connectorAllowed: false
+//                 },
+//                 pointStart: 2018
+//             }
+//         },
 
-        series: year_list,
+//         series: year_list,
 
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 5009
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
+//         responsive: {
+//             rules: [{
+//                 condition: {
+//                     maxWidth: 5009
+//                 },
+//                 chartOptions: {
+//                     legend: {
+//                         layout: 'horizontal',
+//                         align: 'center',
+//                         verticalAlign: 'bottom'
+//                     }
+//                 }
+//             }]
+//         }
 
-    });
+//     });
 
- });
+//  });
