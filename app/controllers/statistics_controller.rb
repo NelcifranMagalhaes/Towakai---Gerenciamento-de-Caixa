eaab21340f6bdd15304f94cb9984f69331a710cf
nelcifran <nelcifranpires@gmail.com>
2019-02-25 16:09:45 -0300

    changes in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,18 +1,26 @@
 class StatisticsController < ApplicationController
 	def index
-		all_sales = Sale.all
+		today_date = Date.today.month
+		all_sales = Sale.where("EXTRACT(MONTH FROM sale_date) = #{today_date}") #Todas as vendas no Mês atual
 		product_ids = Array.new
+		total_value_sales = 0
 		#Pega os Ids das vendas
 		all_sales.each do |sale|
+
 			sale.orders.each do |order|
 				product_ids.push(order.product.id)
+
+				if sale.status_sale.reference != 0
+					total_value_sales = total_value_sales + order.product.quantity * order.quant
+				end
 			end
 		end
-
 		@most_frequent_item = product_ids.uniq.max_by{ |i| product_ids.count( i ) }#acha o id mais vendido
-		@most_frequent_item = Product.find_by(id: @most_frequent_item) #Acha o produto mais vendido
 
-		@sales = all_sales.group_by_month(:sale_date).count
+
+		@value_total_all_sales = total_value_sales
+		@most_frequent_item = Product.find_by(id: @most_frequent_item) #Acha o produto mais vendido
+		@sales = all_sales.group_by_day(:sale_date).count
 
 	end
 end
